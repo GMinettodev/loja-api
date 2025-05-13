c55f2254e7955d21a863db18119790e8e5d03201
Gabriel Andreazza <gabriel.300305@bento.local>
2025-05-13 19:10:27 -0300

    Projeto inicial com ESLint, Prettier e estrutura de API

---
+++ src/database/create_tables.sql
@@ -0,0 +1,10 @@
+USE loja_db;
+
+CREATE TABLE
+    IF NOT EXISTS products (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE,
+        price DECIMAL(10, 2) NOT NULL CHECK (price > 0),
+        stock INT NOT NULL CHECK (stock >= 0),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
